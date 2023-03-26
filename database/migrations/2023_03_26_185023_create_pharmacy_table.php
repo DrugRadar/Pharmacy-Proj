0fc05a6558bb8ce9679c7ea3ee5cd35a37d8a640
Nada Saeed <nada1998saeed@gmail.com>
2023-03-26 21:22:16 +0200

    creating area and pharmacy migrations

--- database/migrations/2023_03_26_185023_create_pharmacy_table.php
+++ database/migrations/2023_03_26_185023_create_pharmacy_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pharmacy', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable('false');
+            $table->unsignedBigInteger('area_id')->nullable('false');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('national_id')->unique();
+            $table->string('avatar_image');
+            $table->foreign('area_id')->references('id')->on('area');
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pharmacy');
     }
 };
