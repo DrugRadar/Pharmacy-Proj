40b987c9a5bcd1c683e94bb0988a44da69a6ede8
marim99 <marim.khaled99@gmail.com>
2023-03-27 15:37:27 +0200

    create area

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Area;
 use Illuminate\Http\Request;
 
 class AreaController extends Controller
 {
     //
+    public function index(){
+        $areas = Area::all();
+        return view('dashboard.area.index',['areas' => $areas]); 
+    }
+    public function create(){
+        return view('dashboard.area.create');
+    }
+    public function store(){
+        Area::create([
+            'name' => request()->name,
+            'address' => request()->address,
+        ]);
+        return to_route('area.index');
+    }
 }
