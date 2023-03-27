1fdaba73649faf25ff3784b07ee08634f1fbb616
Radwa Khalil <radwakhalil22@gmail.com>
2023-03-27 21:05:22 +0200

    Updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,9 +24,8 @@
     <!-- CSS Files -->
     <link id="pagestyle" href="{{URL::asset('css/material-dashboard.css?v=3.0.0') }}" rel="stylesheet" />
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
-    {{-- <link rel="stylesheet" href="css/material-dashboard.css"> --}}
-    <link rel="stylesheet" href="css/DarkToggleStyle.css">
-    <link rel="stylesheet" href="css/logo.css">
+    <link rel="stylesheet" href="{{asset('css/DarkToggleStyle.css')}}">
+    <link rel="stylesheet" href="{{asset('css/logo.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
 
