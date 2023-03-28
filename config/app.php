b9c59c5ac56fbc8f4cd9532e480fd887d88901e5
marim99 <marim.khaled99@gmail.com>
2023-03-28 23:15:37 +0200

    permission seeder

--- config/app.php
+++ config/app.php
@@ -195,6 +195,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Cog\Laravel\Ban\Providers\BanServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
