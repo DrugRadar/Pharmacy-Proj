ab5cf51ebccc169d773609e074c7becc83cb71a4
Nada Saeed <nada1998saeed@gmail.com>
2023-03-29 16:38:11 +0200

    editing database

--- app/Models/Pharmacy.php
+++ app/Models/Pharmacy.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pharmacy extends Model
 {
     use HasFactory;
-    protected $table = 'pharmacy';
+    protected $table = 'pharmacies';
 
     protected $fillable = [
         'name',
