f8ecfe6555247ba49bc712f77847a4b0c5bf9f78
marim99 <marim.khaled99@gmail.com>
2023-03-29 22:20:48 +0200

    permission

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -58,7 +58,7 @@ class DoctorController extends Controller
                 'password' => Hash::make(request()->password),
             ]);
             $newDoctor->user()->save($user);
-            $newDoctor->assignRole(['doctor']);
+            // $newDoctor->assignRole(['doctor']);
         }
         return to_route('doctor.index');
     }
