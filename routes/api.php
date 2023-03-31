5ca5c8c1551b57e8b381130c121d2381102f3736
hager khaled <hagerk720@gmail.com>
2023-03-31 13:29:54 +0200

    free

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('client/{id}', [ClientController::class , 'show'])->name('client.show');
+Route::middleware(['auth:sanctum', 'APIverified'])->get('client/{id}', [ClientController::class , 'show'])->name('client.show');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
