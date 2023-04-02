26ed4ee27d30054b03666d93bfd4e336ecc799c9
Radwa Khalil <radwakhalil22@gmail.com>
2023-04-02 22:10:31 +0200

    default user photo

--- resources/views/dashboard/doctor/profile.blade.php
+++ resources/views/dashboard/doctor/profile.blade.php
@@ -60,13 +60,14 @@
     </style>
 
 <div class="card align-self-center bg-gradient-dark">
-        @if($doctor->getFirstMediaUrl('avatar_image', 'thumb')){
+        {{-- @if($doctor->getFirstMediaUrl('avatar_image', 'thumb')){
             <img src="{{$doctor->getFirstMediaUrl('avatar_image', 'thumb')}}" alt="profile picture" class="profile__picture">
         }
         @else{
             <img src="{{asset('assets/gifs/user.png')}}" alt="profile picture" class="profile__picture">
         }
-        @endif
+        @endif --}}
+        <img src="{{$doctor->getFirstMediaUrl('avatar_image', 'thumb')}}" alt="profile picture" class="profile__picture">
         <div class="text">
             <h3>Doctor Name</h3>
             <p>{{$doctor->name}}</p>
