79fda621c9b00897fb3837e39c19af87629336de
radwa nabil <radwaismail444@gmail.com>
2023-04-03 11:30:15 +0200

    export

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -233,6 +233,16 @@ thead {
                     </a>
                 </li>
                 @endif
+                @if(Auth::user()->hasrole('pharmacy'))
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{route('orders.export')}}">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class='bx bx-export' style="font-size: 25px;"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Export Orders</span>
+                    </a>
+                </li>
+                @endif
             </ul>
         </div>
     </aside>
