5a4c343d58668a98a34640abd17c2df4134d9d91
hager khaled <hagerk720@gmail.com>
2023-04-04 20:08:49 +0200

    ajax

--- resources/views/dashboard/charts/revenue.blade.php
+++ resources/views/dashboard/charts/revenue.blade.php
@@ -12,19 +12,39 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
-var ctx = document.getElementById('revenue-chart').getContext('2d');
-var chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: <?= json_encode($labels) ?>,
-        datasets: [{
-            label: 'Monthly Revenue',
-            data: <?= json_encode($data) ?>,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1
-        }]
-    },
+// var ctx = document.getElementById('revenue-chart').getContext('2d');
+// var chart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: <?= json_encode($labels) ?>,
+//         datasets: [{
+//             label: 'Monthly Revenue',
+//             data: <?= json_encode($data) ?>,
+//             backgroundColor: 'rgba(255, 99, 132, 0.2)',
+//             borderColor: 'rgba(255, 99, 132, 1)',
+//             borderWidth: 1
+//         }]
+//     },
+// });
+$.ajax({
+    url: 'your-url-here',
+    type: 'GET',
+    success: function(data) {
+        var ctx = document.getElementById('revenue-chart').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: JSON.parse(data.labels),
+                datasets: [{
+                    label: 'Monthly Revenue',
+                    data: JSON.parse(data.data),
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }]
+            },
+        });
+    }
 });
 // Fetch gender data from the server
 var ctx = document.getElementById('gender-chart').getContext('2d');
