c11d53407ee76907aecb58484bb366ca6390f6aa
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-04-05 17:32:41 +0200

    Dark Mode
    Co-Authored-By: Nada Saeed <77510429+Nada98Sakr@users.noreply.github.com>

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -3,13 +3,21 @@ let darkmodebtn = document.querySelector("#dark-mode");
 let body = document.querySelector('body');
 let nav  = document.querySelector('nav');
 let navLinkText = document.querySelector(".nav-link-text");
+let i = document.querySelector('i');
+let footer = document.querySelector('footer');
+
 
 const savedValue = localStorage.getItem("site-mode");
 if (savedValue !== null) {
     darkmodebtn.checked = savedValue === "dark";
-    darkmodebtn.checked
-        ? body.classList.add("darkMode")
-        : body.classList.add("lightMode");
+    if(darkmodebtn.checked){
+        body.classList.add("darkMode")
+        footer.classList.add("bg-gradient-dark");
+    }
+    else{
+        body.classList.add("lightMode");
+        footer.classList.remove("bg-gradient-dark");
+    }
 }
 
 darkmodebtn.addEventListener('change', function(){
@@ -20,12 +28,12 @@ darkmodebtn.addEventListener('change', function(){
         body.classList.remove("lightMode");
         body.classList.add("darkMode");
         navLinkText.style.color ='white';
+        footer.classList.add("bg-gradient-dark");
     } else {
-        console.log("Checkbox is not checked");
         body.classList.remove("bg-gray-200");
         body.classList.remove("darkMode");
         body.classList.add("lightMode");
-        
+        footer.classList.remove('bg-gradient-dark');
     }
 
     localStorage.setItem("site-mode", value);
