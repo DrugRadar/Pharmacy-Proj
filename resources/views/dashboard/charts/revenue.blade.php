8ff47d8707216c56ccbb5ac17b491ed2e3c83c3d
hager khaled <hagerk720@gmail.com>
2023-04-05 18:13:47 +0200

    names on chart

--- resources/views/dashboard/charts/revenue.blade.php
+++ resources/views/dashboard/charts/revenue.blade.php
@@ -8,30 +8,11 @@
         <canvas id="gender-chart"></canvas>
         <canvas id="ordersChart"></canvas>
     </div>
-
-
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
-// var revenueChart =  $(document).ready(function() {    
-// // var ctx = document.getElementById('revenue-chart').getContext('2d');
-// $.ajax({
-//     url: '/charts/revenue',
-//     type: 'GET',
-//     dataType: 'json',
-//     success: function(data) {
-//         var chart = new new Chart(document.getElementById('revenue-chart'), {
-//             type: 'line',
-//             data: data,
-//         });
-//         // chart.destroy();
-
-//     }
-
-// });
-// }); 
 $(document).ready(function() {
     $.ajax({
         url: '/charts/revenue',
@@ -86,48 +67,38 @@ var userChart =  $(document).ready(function() {
             type: "GET",
             dataType: "json",
             success: function(data) {
-                var labels = [];
-                var counts = [];
-
-                console.log(data);
-
-                for (var i = 0; i < data.data.length; i++) {
-                    labels.push(data.data[i].client_id);
-                    counts.push(data.data[i].orders);
-                }
-
-                var chartData = {
-                    labels: labels,
-                    datasets: [{
-                        data: counts,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.5)',
-                            'rgba(54, 162, 235, 0.5)',
-                            'rgba(255, 206, 86, 0.5)',
-                            'rgba(75, 192, 192, 0.5)',
-                            'rgba(153, 102, 255, 0.5)',
-                            'rgba(255, 159, 64, 0.5)',
-                            'rgba(255, 99, 132, 0.5)',
-                            'rgba(54, 162, 235, 0.5)',
-                            'rgba(255, 206, 86, 0.5)',
-                            'rgba(75, 192, 192, 0.5)'
-                        ],
-                    }]
-                };
-
-                var options = {
-                    title: {
-                        display: true,
-                        text: 'Top 10 Users by Order Count'
-                    }
-                };
-
-                var chart = new Chart(document.getElementById('ordersChart'), {
-                    type: 'pie',
-                    data: chartData,
-                    options: options
-                });
+            var labels = [];
+            var counts = [];
+            console.log(data);
+            for (var i = 0; i < data.data.length; i++) {
+                labels.push(data.data[i].name);
+                counts.push(data.data[i].orders_count);
             }
+            var chartData = {
+                labels: labels,
+                datasets: [{
+                    data: counts,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.5)',
+                        'rgba(54, 162, 235, 0.5)',
+                        'rgba(255, 206, 86, 0.5)',
+                        'rgba(75, 192, 192, 0.5)',
+                        'rgba(153, 102, 255, 0.5)',
+                        'rgba(255, 159, 64, 0.5)',
+                        'rgba(255, 99, 132, 0.5)',
+                        'rgba(54, 162, 235, 0.5)',
+                        'rgba(255, 206, 86, 0.5)',
+                        'rgba(75, 192, 192, 0.5)'
+                    ],
+                }]
+                
+            };
+            var chart = new Chart(document.getElementById('ordersChart'), {
+                type: 'pie',
+                data: chartData,
+
+            });
+        }
         });
     });
 </script>
